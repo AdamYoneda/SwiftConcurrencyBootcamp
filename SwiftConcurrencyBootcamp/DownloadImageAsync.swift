2a476153a84ad9fb7887c4607229f8f7b516b1f4
Adam Yoneda <adamyone.ttlife@gmail.com>
2023-09-18 18:17:55 +0900

    DownloadImageAsync : escaping closure と completion handlerを利用した画像のダウンロード

--- SwiftConcurrencyBootcamp/DownloadImageAsync.swift
+++ SwiftConcurrencyBootcamp/DownloadImageAsync.swift
@@ -7,9 +7,60 @@
 
 import SwiftUI
 
+class DownloadImageAsyncImageLoader {
+    
+    let url = URL(string: "https://picsum.photos/200")!
+    
+    func downloadWithEscaping(completionHandler: @escaping (_ image: UIImage?, _ error: Error?) -> ()) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let data,
+                let image = UIImage(data: data),
+                let response = response as? HTTPURLResponse,
+                response.statusCode >= 200 && response.statusCode < 300 else {
+                completionHandler(nil, error)
+                return
+            }
+            
+            completionHandler(image, nil)
+        }
+        .resume()
+    }
+    
+}
+
+
+class DownloadImageAsyncViewModel: ObservableObject {
+    
+    @Published var image: UIImage? = nil
+    let loader = DownloadImageAsyncImageLoader()
+    
+    func fetchImage() {
+        loader.downloadWithEscaping { [weak self] image, error in
+            DispatchQueue.main.async {
+                self?.image = image
+            }
+        }
+    }
+    
+}
+
 struct DownloadImageAsync: View {
+    
+    @StateObject private var viewModel = DownloadImageAsyncViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            if let image = viewModel.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 250, height: 250)
+            }
+        }
+        .onAppear {
+            viewModel.fetchImage()
+        }
     }
 }
 
