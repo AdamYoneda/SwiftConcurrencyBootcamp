66e249791aa31c34a9994f2a13bdf44dbd99bae7
Adam Yoneda <adamyone.ttlife@gmail.com>
2023-09-18 17:35:19 +0900

    DownloadImageAsync : ファイル作成

---
+++ SwiftConcurrencyBootcamp/DownloadImageAsync.swift
@@ -0,0 +1,20 @@
+//
+//  DownloadImageAsync.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Adam Yoneda on 2023/09/18.
+//
+
+import SwiftUI
+
+struct DownloadImageAsync: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DownloadImageAsync_Previews: PreviewProvider {
+    static var previews: some View {
+        DownloadImageAsync()
+    }
+}
