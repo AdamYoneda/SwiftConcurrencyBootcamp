0f4b7d1f3fa009d60324162d99a420967bd79c4f
Adam Yoneda <adamyone.ttlife@gmail.com>
2023-09-25 18:57:34 +0900

    AsyncAwaitBootcamp : swift Concurrency以前のバックグラウンドスレッドを利用した非同期処理

--- SwiftConcurrencyBootcamp/AsyncAwaitBootcamp.swift
+++ SwiftConcurrencyBootcamp/AsyncAwaitBootcamp.swift
@@ -7,9 +7,43 @@
 
 import SwiftUI
 
+class AsyncAwaitBootcampViewModel: ObservableObject {
+    
+    @Published var dataArray: [String] = []
+    
+    func addTitle1() {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.dataArray.append("Title1 : \(Thread.current)")
+        }
+    }
+    
+    func addTitle2() {
+        DispatchQueue.global().asyncAfter(deadline: .now() + 2) {
+            let title = "Title2 : \(Thread.current)"
+            DispatchQueue.main.async {
+                self.dataArray.append(title)
+                
+                let title3 = "Title3 : \(Thread.current)"
+                self.dataArray.append(title3)
+            }
+        }
+    }
+}
+
 struct AsyncAwaitBootcamp: View {
+    
+    @StateObject private var viewModel = AsyncAwaitBootcampViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(viewModel.dataArray, id: \.self) { data in
+                Text(data)
+            }
+        }
+        .onAppear(perform: {
+            viewModel.addTitle1()
+            viewModel.addTitle2()
+        })
     }
 }
 
