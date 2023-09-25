a3ff6758e3e8810066d5b62b7a3a4da51c26f2d5
Adam Yoneda <adamyone.ttlife@gmail.com>
2023-09-25 18:12:59 +0900

    AsyncAwaitBootcamp: ファイル作成

---
+++ SwiftConcurrencyBootcamp/AsyncAwaitBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncAwaitBootcamp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Adam Yoneda on 2023/09/25.
+//
+
+import SwiftUI
+
+struct AsyncAwaitBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncAwaitBootcamp()
+}
