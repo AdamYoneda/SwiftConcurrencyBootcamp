59661f7566f3dc7f5bae21eadc1ac00de1b9a22c
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-07-03 12:50:33 +0900

    update: ViewModelの作成

--- SwiftConcurrencyBootcamp/TaskBootcamp.swift
+++ SwiftConcurrencyBootcamp/TaskBootcamp.swift
@@ -7,7 +7,26 @@
 
 import SwiftUI
 
+class TaskBootcampViewModel: ObservableObject {
+    
+    @Published var image: UIImage? = nil
+    
+    func fetchImage() async {
+        do {
+            guard let url = URL(string: "https://picsum.photos/200") else { return }
+            let (data, _) = try await URLSession.shared.data(from: url, delegate: nil)
+            
+            self.image = UIImage(data: data)
+        } catch {
+            print(#function, error.localizedDescription)
+        }
+    }
+}
+
 struct TaskBootcamp: View {
+    
+    @StateObject private var viewModel = TaskBootcampViewModel()
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
