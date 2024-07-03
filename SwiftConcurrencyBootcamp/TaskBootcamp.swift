a201d9b7e3df87aa7fc914fe9d0139b72d302687
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-07-03 12:59:08 +0900

    update: fetchImage2という関数を作成し、View内でトリガー

--- SwiftConcurrencyBootcamp/TaskBootcamp.swift
+++ SwiftConcurrencyBootcamp/TaskBootcamp.swift
@@ -10,6 +10,7 @@ import SwiftUI
 class TaskBootcampViewModel: ObservableObject {
     
     @Published var image: UIImage? = nil
+    @Published var image2: UIImage? = nil
     
     func fetchImage() async {
         do {
@@ -17,6 +18,17 @@ class TaskBootcampViewModel: ObservableObject {
             let (data, _) = try await URLSession.shared.data(from: url, delegate: nil)
             
             self.image = UIImage(data: data)
+        } catch {
+            print(#function, error.localizedDescription)
+        }
+    }
+    
+    func fetchImage2() async {
+        do {
+            guard let url = URL(string: "https://picsum.photos/1000") else { return }
+            let (data, _) = try await URLSession.shared.data(from: url, delegate: nil)
+            
+            self.image2 = UIImage(data: data)
         } catch {
             print(#function, error.localizedDescription)
         }
@@ -35,10 +47,17 @@ struct TaskBootcamp: View {
                     .scaledToFit()
                     .frame(width: 200, height: 200)
             }
+            if let image = viewModel.image2 {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
         })
         .onAppear(perform: {
             Task {
                 await viewModel.fetchImage()
+                await viewModel.fetchImage2()
             }
         })
     }
