7bc49166c417b95f2f48e6382c2ccba0d2fd5840
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-07-05 16:29:11 +0900

    update: AsyncLetBootcamp.swiftの追加

---
+++ SwiftConcurrencyBootcamp/AsyncLetBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  AsyncLetBootcamp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Adam Yoneda on 2024/07/05.
+//
+
+import SwiftUI
+
+struct AsyncLetBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AsyncLetBootcamp()
+}
