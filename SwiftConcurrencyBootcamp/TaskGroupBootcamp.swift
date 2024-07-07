52cce75abf6592f93def0678650bab71898b0944
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-07-07 17:54:21 +0900

    add: TaskGroupBootcampファイルの追加

---
+++ SwiftConcurrencyBootcamp/TaskGroupBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  TaskGroupBootcamp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Adam Yoneda on 2024/07/09.
+//
+
+import SwiftUI
+
+struct TaskGroupBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TaskGroupBootcamp()
+}
