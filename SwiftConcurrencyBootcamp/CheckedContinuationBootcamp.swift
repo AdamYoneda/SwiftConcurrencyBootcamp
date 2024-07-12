ae09b872d863523eb287098e420786f392ada211
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-07-12 20:40:05 +0900

    update: URL Stringから画像をDLして表示させるコードを用意

--- SwiftConcurrencyBootcamp/CheckedContinuationBootcamp.swift
+++ SwiftConcurrencyBootcamp/CheckedContinuationBootcamp.swift
@@ -7,9 +7,58 @@
 
 import SwiftUI
 
+class CheckedContinuationBootcampNetworkManager {
+    
+    func getData(url: URL) async throws -> Data {
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url, delegate: nil)
+            return data
+        } catch {
+            throw error
+        }
+    }
+    
+}
+
+class CheckedContinuationBootcampViewModel: ObservableObject {
+    
+    @Published var image: UIImage? = nil
+    let networkManager = CheckedContinuationBootcampNetworkManager()
+    
+    func getImage() async {
+        guard let url = URL(string: "https://picsum.photos/300") else { return }
+        do {
+            let data = try await networkManager.getData(url: url)
+            
+            if let image = UIImage(data: data) {
+                await MainActor.run {
+                    self.image = image
+                }
+            } else {
+                print("-------")
+            }
+        } catch {
+            print(#function, error)
+        }
+    }
+}
+
 struct CheckedContinuationBootcamp: View {
+    
+    @StateObject private var viewModel = CheckedContinuationBootcampViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            if let image = viewModel.image {
+                Image(uiImage: image)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+        }
+        .task {
+            await viewModel.getImage()
+        }
     }
 }
 
