c2f15e2d66f8ac25c2db994741d97de0c118435b
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-07-12 19:21:05 +0900

    add: CheckedContinuationBootcamp.swift

---
+++ SwiftConcurrencyBootcamp/CheckedContinuationBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  CheckedContinuationBootcamp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Adam Yoneda on 2024/07/12.
+//
+
+import SwiftUI
+
+struct CheckedContinuationBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CheckedContinuationBootcamp()
+}
