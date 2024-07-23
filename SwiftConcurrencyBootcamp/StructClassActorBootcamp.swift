34c69789f1d9f4dde64c3fb0c4a3e616b389a06f
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-07-23 17:08:35 +0900

    update: StructClassActorBootcampの画面が表示された時の簡単なテストコードを追加

--- SwiftConcurrencyBootcamp/StructClassActorBootcamp.swift
+++ SwiftConcurrencyBootcamp/StructClassActorBootcamp.swift
@@ -10,9 +10,19 @@ import SwiftUI
 struct StructClassActorBootcamp: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .onAppear(perform: {
+                self.runTest()
+            })
     }
 }
 
 #Preview {
     StructClassActorBootcamp()
+}
+
+extension StructClassActorBootcamp {
+    
+    private func runTest() {
+        print("--- Test Started ---")
+    }
 }
