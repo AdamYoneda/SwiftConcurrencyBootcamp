afc10564819feec819056737923239890e1793c3
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-07-23 18:04:57 +0900

    update: structのテストを行うコードを記述(~11:25)

--- SwiftConcurrencyBootcamp/StructClassActorBootcamp.swift
+++ SwiftConcurrencyBootcamp/StructClassActorBootcamp.swift
@@ -20,9 +20,29 @@ struct StructClassActorBootcamp: View {
     StructClassActorBootcamp()
 }
 
+struct MyStruct {
+    var title: String
+}
+
 extension StructClassActorBootcamp {
     
     private func runTest() {
-        print("--- Test Started ---")
+        print("//* Test Started *//")
+        structTest1()
+    }
+    
+    private func structTest1() {
+        let objectA = MyStruct(title: "Starting title!")
+        print("ObjectA: \(objectA.title)")
+        
+        var objectB = objectA
+        print("ObjectB: \(objectB.title)")
+        
+        // titleを変更
+        objectB.title = "Second title!!"
+        print("---- ObjectB title Changed ---")
+        
+        print("ObjectA: \(objectA.title)")
+        print("ObjectB: \(objectB.title)")
     }
 }
