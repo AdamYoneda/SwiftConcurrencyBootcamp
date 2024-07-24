cf30637498d2bd7179bf09cad7138ec0fa90b900
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-07-24 15:24:59 +0900

    update: classの参照渡しを確認するコードを追加

--- SwiftConcurrencyBootcamp/StructClassActorBootcamp.swift
+++ SwiftConcurrencyBootcamp/StructClassActorBootcamp.swift
@@ -24,11 +24,20 @@ struct MyStruct {
     var title: String
 }
 
+class MyClass {
+    var title: String
+    
+    init(title: String) {
+        self.title = title
+    }
+}
+
 extension StructClassActorBootcamp {
     
     private func runTest() {
         print("//* Test Started *//")
-        structTest1()
+        //        structTest1()
+        classTest1()
     }
     
     private func structTest1() {
@@ -36,6 +45,21 @@ extension StructClassActorBootcamp {
         print("ObjectA: \(objectA.title)")
         
         var objectB = objectA
+        print("ObjectB: \(objectB.title)")
+        
+        // titleを変更
+        objectB.title = "Second title!!"
+        print("---- ObjectB title Changed ---")
+        
+        print("ObjectA: \(objectA.title)")
+        print("ObjectB: \(objectB.title)")
+    }
+    
+    private func classTest1() {
+        let objectA = MyClass(title: "Starting title!")
+        print("ObjectA: \(objectA.title)")
+        
+        let objectB = objectA
         print("ObjectB: \(objectB.title)")
         
         // titleを変更
