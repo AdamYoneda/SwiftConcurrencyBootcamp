f7bec38cd352a4c0f3431c25c28da3eb4f79133e
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-08-02 15:32:39 +0900

    add: ActorsBootcamp.swift

---
+++ SwiftConcurrencyBootcamp/ActorsBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  ActorsBootcamp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Adam Yoneda on 2024/08/02.
+//
+
+import SwiftUI
+
+struct ActorsBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ActorsBootcamp()
+}
