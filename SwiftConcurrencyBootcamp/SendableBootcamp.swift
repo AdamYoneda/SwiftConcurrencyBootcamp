58c1b1c3a059a65f0dc69d91b49a921fd35e3f30
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-08-11 19:22:04 +0900

    add: SendableBootcamp.swiftファイルの追加

---
+++ SwiftConcurrencyBootcamp/SendableBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  SendableBootcamp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Adam Yoneda on 2024/08/11.
+//
+
+import SwiftUI
+
+struct SendableBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SendableBootcamp()
+}
