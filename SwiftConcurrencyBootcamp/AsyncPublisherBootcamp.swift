ab67938f7d1759c3e689761972cb70ccd5501966
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-08-14 21:37:00 +0900

    update: MVVM構造に

--- SwiftConcurrencyBootcamp/AsyncPublisherBootcamp.swift
+++ SwiftConcurrencyBootcamp/AsyncPublisherBootcamp.swift
@@ -7,7 +7,22 @@
 
 import SwiftUI
 
+actor AsyncPublisherDataManager {
+    
+    @Published var myData: [String] = []
+    
+}
+
+class AsyncPublisherBootcampViewModel: ObservableObject {
+    
+    @Published var dataArray: [String] = []
+    
+}
+
 struct AsyncPublisherBootcamp: View {
+    
+    @StateObject private var viewModel = AsyncPublisherBootcampViewModel()
+    
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
