fdc88a31faee2e7fd96a77c57210df6e5d56d964
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-08-19 17:30:34 +0900

    add: StrongSelfBootcamp.swift

---
+++ SwiftConcurrencyBootcamp/StrongSelfBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  StrongSelfBootcamp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Adam Yoneda on 2024/08/19.
+//
+
+import SwiftUI
+
+struct StrongSelfBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StrongSelfBootcamp()
+}
