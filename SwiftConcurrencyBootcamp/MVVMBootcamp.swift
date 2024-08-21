edb5e73b4ed143f5ca38bed67b3bc7744f51fedc
Adam Yoneda <112159367+AdamYoneda@users.noreply.github.com>
2024-08-21 15:14:42 +0900

    add: MVVMBootcamp.swift

---
+++ SwiftConcurrencyBootcamp/MVVMBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  MVVMBootcamp.swift
+//  SwiftConcurrencyBootcamp
+//
+//  Created by Adam Yoneda on 2024/08/21.
+//
+
+import SwiftUI
+
+struct MVVMBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MVVMBootcamp()
+}
